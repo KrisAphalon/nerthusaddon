10f2b1380983a8d40d43d5c35a99a54fa97b94d5
KrisAphalon <Krzysztof.Kacz@interia.pl>
2020-03-14 18:35:11 +0100

    Update nerthus_addon.user.js

--- nerthus_addon.user.js
+++ nerthus_addon.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Nerthus Addon
 // @namespace    http://www.margonem.pl/
-// @version      2.10.6
+// @version      2.11.3
 // @description  Addon for Nerthus
 // @author       Aldi, Kris Aphalon
 // @match        http://nerthus.margonem.pl/
@@ -10,7 +10,7 @@
 (function ()
 {
     const script = document.createElement('script')
-    script.src = 'https://akrzyz.github.io/nerthusaddon/NN_start.js'
+    script.src = 'https://KrisAphalon.github.io/nerthusaddon/NN_start.js'
     script.async = false
     document.head.appendChild(script)
 })()
